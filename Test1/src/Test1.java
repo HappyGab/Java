45b786d4b76ddf07e6d52ca5122e6c27ed931e15
HappyGab <105558236+HappyGab@users.noreply.github.com>
2022-05-22 14:03:50 -0400

    nothing important

--- Test1/src/Test1.java
+++ Test1/src/Test1.java
@@ -1,8 +1,16 @@
 
 public class Test1 {
 
-	
+
 	/*
 	 * does this shit work?
 	 */
+	public static void main(String[] args) {
+
+		boolean isOkay = true;
+
+		if (isOkay) System.out.println ("Statement #1");
+
+		else System.out.println ("Statement #2");
+	}
 }
