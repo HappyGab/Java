a32f947793921127a1f5e113c29a907e6f781608
HappyGab <105558236+HappyGab@users.noreply.github.com>
2022-06-03 09:51:07 -0400

    Lab 2 done w/o bonus yet

--- pTriangle/src/pTriangle/pascalTriangle.java
+++ pTriangle/src/pTriangle/pascalTriangle.java
@@ -6,7 +6,7 @@ public class pascalTriangle {
 	
 	public static void main(String[] args) {
 		
-		for (int i = 0; i<10; i++) {
+		for (int i = 0; i<15; i++) {
 			
 			triangle Pasc = new triangle(i, arr);
 					
