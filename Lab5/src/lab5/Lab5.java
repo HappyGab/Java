50d0bd0baa706263150a7abf672b99d3b393dce3
HappyGab <105558236+HappyGab@users.noreply.github.com>
2022-07-23 23:27:56 -0400

    all done with the javadoc

--- Lab5/src/lab5/Lab5.java
+++ Lab5/src/lab5/Lab5.java
@@ -1,48 +1,89 @@
+/*
+ * Gabriel Koscielniak
+ * #041062261
+ * 
+ * CST8132
+ * Professor Daniel Cormier
+ * 
+ * Lab 5
+ * 
+ * Bread recipe manager
+ * 
+ */
+
+
 package lab5;
 
 import java.util.Scanner;
 
+/**
+ * Driver Class
+ * 
+ * @author gabko
+ * @version 1.0
+ * @since 1.8
+ */
 public class Lab5 {
 
+	//RecipeManager object
 	static RecipeManager rM = new RecipeManager();
 
+	//keyboard scanner
 	static Scanner keyboard = new Scanner(System.in);
 	
+	/**
+	 * Main method, makes the program run
+	 * 
+	 * @param args : nothing
+	 */
 	public static void main(String[] args) {
 
+		//starts the program
 		Lab5 Lab5 = new Lab5();
 		System.out.println("Welcome to the world's best bread recipe manager.");
 
+		//boolean, false : does not quit the program, true : quits the program
 		boolean quit = false;
+		
+		//int for the user's input
 		int playerIn = 0;
 		
+		//keeps looping until quit = true
 		while (quit == false) {
 			
-
+			//prints the menu
 			Lab5.printMenu();
 			
+			//checks for exceptions
 			try {
+				//user's input
 				playerIn = keyboard.nextInt();
 			}
 			catch ( java.util.InputMismatchException e) {
 				
+				//flushes the scanner
 				String s = keyboard.nextLine();
 				System.out.println("\nPlease enter a valid input. \nValid inputs are numbers form 0 to 4");
 			}
 			
+			//options depending on the user's input
 			if (playerIn == 0) {
 				//nothing happens here, have a good day :D
 			}
 			else if (playerIn == 1) {
+				//calls the method that prints the list of available recipes
 				rM.printRlist();
 			}
 			else if (playerIn == 2) {
+				//calls the method to create the list
 				Lab5.createSlist();
 			}
 			else if (playerIn == 3) {
+				//calls the method that prints the shopping list
 				rM.printSlist();
 			}
 			else if (playerIn == 4) {
+				//quits the program
 				System.out.println("\nMade by Gabriel Koscielniak");
 				quit = true;
 			}
@@ -51,9 +92,12 @@ public class Lab5 {
 		}
 	}
 
-
+	/**
+	 * This method prints the option menu
+	 */
 	public void printMenu() {
 
+		//prints the menu
 		System.out.println("\nSelect one of the following options");
 		System.out.println("1. Show available recipes");
 		System.out.println("2. Create shopping list");
@@ -63,21 +107,30 @@ public class Lab5 {
 		System.out.print("Please enter your choice: ");
 	}
 	
+	/**
+	 * This method asks the user for the type and amount of bread to add to the shopping list
+	 * calls the method that create the shopping list with the parameters given by the user
+	 */
 	public void createSlist() {
 		
+		//ints for the user's input
 		int PlayerInBread = 0;
 		int PlayerInNum = 0;
 		
+		//to end the loop
 		boolean loop = false;
 		
 		do{
+			//checks for exceptions
 			try {
+				//gets user's input for the bread wanted
 				loop = false;
 				System.out.print("\nWhich bread would you like? ");
 				PlayerInBread = keyboard.nextInt();	
 			}
 			catch ( java.util.InputMismatchException e) {
-			
+				
+				//tells the user he messed up and flushes the scanner
 				System.out.println("\nPlease enter a valid input. \nValid inputs are numbers form 1 to 7");
 				String s = keyboard.nextLine();
 				loop = true;
@@ -85,16 +138,19 @@ public class Lab5 {
 		}
 		while (loop == true);
 		
+		//to end the second loop
 		boolean loop2 = false;
 		
 		do {
 			try {
+				//gets the user input for the amount of bread
 				loop2 = false;
 				System.out.print("\nHow many would you like? ");
 				PlayerInNum = keyboard.nextInt();	
 			}
 			catch ( java.util.InputMismatchException e) {
 				
+				//tells the user he messed up and flushes the scanner
 				System.out.println("\nPlease enter a valid input. \nValid inputs are numbers");
 				String s = keyboard.nextLine();
 				loop2 = true;
@@ -102,6 +158,7 @@ public class Lab5 {
 		}
 		while (loop2 == true);
 		
+		//calls the method that add the right bread and amount to the shopping list
 		rM.addToShoppingList(PlayerInBread, PlayerInNum);
 	
 	}
--- Lab5/src/lab5/Recipe.java
+++ Lab5/src/lab5/Recipe.java
@@ -1,5 +1,24 @@
+/*
+ * Gabriel Koscielniak
+ * #041062261
+ * 
+ * CST8132
+ * Professor Daniel Cormier
+ * 
+ * Lab 5
+ * 
+ * Bread recipe manager
+ * 
+ */
+
 package lab5;
 
+/**
+ * 
+ * @author gabko
+ * @version 1.0
+ * @since 1.8
+ */
 public class Recipe {
 
 	public float yeast = 0;
@@ -12,71 +31,141 @@ public class Recipe {
 	
 	public String breadName = "";
 	
+	/**
+	 * sets the name of the bread
+	 * 
+	 * @param name : (string) the name of the bread
+	 */
 	public void setName(String name) {	
 		this.breadName = name;
 	
 	}
 	
+	/**
+	 * sets the number of breads
+	 * 
+	 * @param breadNum : (int) the number of breads
+	 */
 	public void setBreadNum(int breadNum) {	
 		this.breadNum = breadNum;
 		
 	}
 	
+	/**
+	 * sets the amount of yeast
+	 * 
+	 * @param num : (int) the amount of yeast
+	 */
 	public void setYeast(float num) {
 		this.yeast = num * breadNum;
 		
 	}
 	
+	/**
+	 * sets the amount of flour
+	 * 
+	 * @param num : (int) the amount of flour
+	 */
 	public void setFlour(float num) {
 		this.flour = num * breadNum;
 		
 	}
 	
+	/**
+	 * sets the amount of sugar
+	 * 
+	 * @param num : (int) the amount of sugar
+	 */
 	public void setSugar(float num) {
 		this.sugar = num * breadNum;
 		
 	}
 	
+	/**
+	 * sets the amount of eggs
+	 * 
+	 * @param num : (int) the amount of eggs
+	 */
 	public void setEggs(float num) {
 		this.eggs = num * breadNum;
 		
 	}
 	
+	/**
+	 * sets the amount of butter
+	 * 
+	 * @param num : (int) the amount of butter
+	 */
 	public void setButter(float num) {
 		this.butter = num * breadNum;
 		
 	}
 	
+	/**
+	 * gets the bread's name
+	 * 
+	 * @return breadName: (string) the bread's name
+	 */
 	public String getName() {
 		
 		return breadName;
 	}
 	
+	/**
+	 * gets the number of breads
+	 * 
+	 * @return breadNum: (int) the number of breads
+	 */
 	public int getBreadNum() {
 		
 		return breadNum;
 	}
 	
+	/**
+	 * gets the amount of yeast
+	 * 
+	 * @return yeast: (float) the amount of yeast
+	 */
 	public float getYeast() {
 		
 		return yeast;
 	}
 
+	/**
+	 * gets the amount of flour
+	 * 
+	 * @return yeast: (float) the amount of flour
+	 */
 	public float getFlour() {
 		
 		return flour;
 	}
 	
+	/**
+	 * gets the amount of sugar
+	 * 
+	 * @return yeast: (float) the amount of sugar
+	 */
 	public float getSugar() {
 			
 		return sugar;
 	}
 	
+	/**
+	 * gets the amount of eggs
+	 * 
+	 * @return yeast: (float) the amount of eggs
+	 */
 	public float getEggs() {
 		
 		return eggs;
 	}
 	
+	/**
+	 * gets the amount of butter
+	 * 
+	 * @return yeast: (float) the amount of butter
+	 */
 	public float getButter() {
 		
 		return butter;
